8b27654ec579a43b13c0ca38bf2c9449563ff836
ajipon-44 <iyonegaki@gmail.com>
2021-10-16 15:02:17 +0900

    モデル変更(生徒と先生追加，実ユーザー削除)

--- app/models/real_user.rb
+++
@@ -1,2 +0,0 @@
-class RealUser < ApplicationRecord
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_13_084329) do
+ActiveRecord::Schema.define(version: 2021_10_16_055931) do
 
   create_table "follows", force: :cascade do |t|
     t.integer "follow_user_id"
@@ -63,6 +63,20 @@ ActiveRecord::Schema.define(version: 2021_10_13_084329) do
   create_table "retweets", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
