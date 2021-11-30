20af0b1c5f22b230addf7307fc3f4b8cf05ebf07
ajipon-44 <iyonegaki@gmail.com>
2021-11-30 17:29:15 +0900

    showページ完成

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,20 @@
 class StudentsController < ApplicationController
+  before_action :set_post, only: [:show]
+  before_action :set_replieds, only: [:show]
+
   def index
-    @posts = Post.all
+    @posts = Post.all.order(date: :desc)
+  end
+
+  def show
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def set_replieds
+    @replieds = Post.find(params[:id]).replieds
   end
 end
