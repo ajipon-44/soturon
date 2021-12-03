fcd1dedaa84a94f4bc9c9f3398ae86033d9f3952
ajipon-44 <iyonegaki@gmail.com>
2021-12-03 16:14:01 +0900

    デザイン完成

--- app/controllers/student/posts_controller.rb
+++ app/controllers/student/posts_controller.rb
@@ -1,6 +1,7 @@
 class Student::PostsController < ApplicationController
   before_action :set_post, only: [:show]
   before_action :set_replieds, only: [:show]
+  before_action :set_action, only: [:index, :show]
 
   def index
     @posts = Post.all.order(date: :desc)
@@ -16,5 +17,14 @@ class Student::PostsController < ApplicationController
 
   def set_replieds
     @replieds = Post.find(params[:id]).replieds
+  end
+
+  def set_action
+    @action = params[:action]
+    if @action == "index"
+      @theme = "ホーム"
+    elsif @action == "show"
+      @theme = "投稿"
+    end
   end
 end
